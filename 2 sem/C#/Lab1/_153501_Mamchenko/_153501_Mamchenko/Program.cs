f301c9d902b50bac70baa3f6f9531e656fdac2c6
Ainz <kmamcenko@gmail.com>
2022-03-05 17:19:42 +0300

    C# update

---
+++ 2 sem/C#/Lab1/_153501_Mamchenko/_153501_Mamchenko/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _153501_Mamchenko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the 1st number: ");
+            int firstNumer = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the 2nd number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int quotient = firstNumer / secondNumber;
+            Console.WriteLine("The result of dividing two numbers: {0}", quotient);
+        }
+    }
+}
